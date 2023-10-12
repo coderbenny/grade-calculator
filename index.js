ecaa897154084efd6ddf19dcd6f3793d75326621
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-10-12 20:10:41 +0300

    updated code for error handling

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const inputContainer = document.getElementById("enter-marks");
 const resultContainer = document.getElementById("grade");
+const clickButton = document.getElementById("checkButton")
 
-inputContainer.addEventListener("input", calculateGrade);
+// inputContainer.addEventListener("input", calculateGrade);
+clickButton.addEventListener("click", calculateGrade);
 
 function calculateGrade() {
-    const input = inputContainer.value;
+    const input = parseFloat(inputContainer.value);
   
-    if (isNaN(input) || input < 0 || input > 100) {
-      alert("Invalid input. Please enter valid marks between 0 and 100.");
-      return;
-    }
-  
-    let grade;
-    if (input > 79) {
-        grade = 'A';
-    } else if (input >= 60 && input < 80) {
-        grade = 'B';
-    } else if (input >= 50 && input <60) {
-        grade = 'C';
-    } else if (input >= 40) {
-        grade = 'D';
+    if (isNaN(input) || (input < 0) || (input > 100)) {
+      resultContainer.textContent = "Invalid input. Please enter valid marks between 0 and 100.";
+      resultContainer.style.fontSize = "50px";
     } else {
-        grade = 'E';
+        let grade;
+        if (input > 79) {
+            grade = 'A';
+        } else if (input >= 60) {
+            grade = 'B';
+        } else if (input >= 50) {
+            grade = 'C';
+        } else if (input >= 40) {
+            grade = 'D';
+        } else {
+            grade = 'E';
+        }
+
+        resultContainer.textContent = `${grade}`;
+        resultContainer.style.fontSize = "50px";
     }
-
-    resultContainer.textContent = `${grade}`;
-    resultContainer.style.fontSize = "100px";
-
 }
